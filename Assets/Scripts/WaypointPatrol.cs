83e012147b003ae56bf312dec189a4460b89acc6
Code-MNA <03mna.code@gmail.com>
2022-10-24 21:05:42 -0400

    Added WaypointScript

---
+++ Assets/Scripts/WaypointPatrol.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Patrol : MonoBehaviour
+{
+
+    float cooldownBetweenPoints;
+
+    [SerializeField]
+    List<Transform> transformList;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
