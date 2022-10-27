705bb26d84c944fa8704b5e521a32b63195e8521
Code-MNA <03mna.code@gmail.com>
2022-10-27 06:41:47 -0400

    added black door

--- Assets/Scripts/Rotate.cs
+++ Assets/Scripts/Rotate.cs
@@ -2,17 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Rotate : MonoBehaviour
 {
     [SerializeField] Light objectLight;
+    MeshRenderer meshRenderer;
+    TextMesh textIndicator;
     private void Start()
     {
+        meshRenderer = GetComponent<MeshRenderer>();
         objectLight = GetComponent<Light>();
-        objectLight.color = GetComponent<MeshRenderer>().material.color;
+        objectLight.color = meshRenderer.material.color;
+
     }
     // Update is called once per frame
     void Update()
     {
         transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
     }
+    
 }
